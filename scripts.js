bcc8370c3c71b723a55a92c52a5aa0e1c5239ace
SUU2237 <siayu1030@gmail.com>
2024-11-24 16:56:01 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -130,7 +130,6 @@ function generateRandomCombo() {
 // 將隨機搭配加入購物車
 function addRandomToCart() {
     if (currentCombo) {
-        ////currentCombo.items.forEach(item => cart.push({ item, price: currentCombo.total }));
         cart.push({currentCombo.items, currentCombo.total});
         totalPrice += currentCombo.total;
         cartCount += currentCombo.items.length;
